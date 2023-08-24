277614548ff259b82cc2268ac17f6e7b2b6dcea6
SESA535431 <vinod.bathula@se.com>
2023-08-24 11:06:58 +0530

    Cors and CSRF disabled

--- src/main/java/com/schneiderelectric/dces/semtech/security/UmsSecurityConfig.java
+++ src/main/java/com/schneiderelectric/dces/semtech/security/UmsSecurityConfig.java
@@ -11,7 +11,9 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 @RequiredArgsConstructor
@@ -45,6 +47,13 @@ public class UmsSecurityConfig {
 
 
         return http.build();
+    }
+
+    @Bean
+    SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
+        serverHttpSecurity
+                .cors(ServerHttpSecurity.CorsSpec::disable);
+        return serverHttpSecurity.build();
     }
 
 
