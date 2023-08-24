4ea297fce859c7993672e103793db54cd45ba092
SESA557572 <mahadeva.prasad1@se.com>
2023-08-24 09:00:55 +0530

    Added distribution Management

--- src/main/java/com/schneiderelectric/dces/semtech/security/CorsConfiguration.java
+++ src/main/java/com/schneiderelectric/dces/semtech/security/CorsConfiguration.java
@@ -4,9 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @Configuration
-public class CorsConfiguration {
+public class CorsConfiguration extends WebMvcConfigurationSupport{
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
@@ -15,9 +16,11 @@ public class CorsConfiguration {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins(
-                                "*")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS");
+                        .allowedOrigins("*")
+                        .allowedHeaders("Access-Control-Allow-Origin","Content-Type","X-Requested-With"
+                                ,"accept","Origin","Access-Control-Request-Method","Access-Control-Request-Headers", "Authorization","token")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
+                        .allowCredentials(true);
             }
         };
     }
