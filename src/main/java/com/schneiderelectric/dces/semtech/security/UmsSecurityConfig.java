a35330db1ba08561cd0f770f53cb2ac7c70bb103
SESA535431 <vinod.bathula@se.com>
2023-08-24 12:25:28 +0530

    Cors and CSRF disabled

--- src/main/java/com/schneiderelectric/dces/semtech/security/UmsSecurityConfig.java
+++ src/main/java/com/schneiderelectric/dces/semtech/security/UmsSecurityConfig.java
@@ -7,17 +7,12 @@ import com.schneiderelectric.dces.semtech.security.filter.CustomOpaqueTokenIntro
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
-import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
@@ -35,7 +30,7 @@ public class UmsSecurityConfig {
 
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .cors(c-> c.configurationSource(corsConfigurationSource()))
+                .cors(Customizer.withDefaults())
                 .formLogin(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(umsSettingProperties.getAllowedUrls()).permitAll()
@@ -53,16 +48,7 @@ public class UmsSecurityConfig {
         return http.build();
     }
 
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("*"));
-        configuration.setAllowedMethods(Arrays.asList("POST","GET","PUT","OPTIONS","DELETE","PATCH"));
-        configuration.setAllowedHeaders(List.of("Access-Control-Allow-Origin","Content-Type","X-Requested-With"
-                ,"accept","Origin","Access-Control-Request-Method","Access-Control-Request-Headers", "Authorization","token"));
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
+
 
 
 }
